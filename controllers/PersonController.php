3f0b901a7bfacb55e4d6439829ff08047dd41c3e
Manop Kongoon <kongoon@hotmail.com>
2015-03-10 13:09:08 +0700

    ปรับปรุง PersonController

--- controllers/PersonController.php
+++ controllers/PersonController.php
@@ -2,7 +2,10 @@
 namespace app\controllers;
 use yii\web\Controller;
 
+//ชื่อ Class ตามด้วย Controller
 class PersonController extends Controller{
+    //action ที่ render view ต้องมี action นำหน้า
+    //
     public function actionTest(){
         $name = "มานพ กองอุ่น";
         return $this->render('test',['myname'=>$name]);
