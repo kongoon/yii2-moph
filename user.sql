d365a48151834303921289e83235849a071dfc72
Manop Kongoon <kongoon@hotmail.com>
2015-03-11 11:41:28 +0700

    Update user.sql

--- user.sql
+++ user.sql
@@ -1,24 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Mar 11, 2015 at 05:30 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `yii2-training`
---
 
 -- --------------------------------------------------------
 
@@ -39,7 +18,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `updated_at` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
@@ -51,6 +30,4 @@ INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_res
 (2, 'demo', '6Tbf6TLS3umfBZ_HWA_kyIUrVzx9G7YE', '$2y$13$u1RJqMnIH4eBhw0IU9WI9OJHBeLJwcCnxISmw0wYWzAfr4IEU6Qha', NULL, 'demo@demo.com', 5, 1, 1420214966, 1420214966),
 (3, 'manop', 'hCRXcGuuKBodC5bxUbg0Np3_D5Q5etqh', '$2y$13$uEnMRURV6VuVqdJH7H02NO8QuMHiX5EYbLZbCo3bvx9omDvGacDu.', NULL, 'kongoon@gmail.com', 1, 1, 0, 0);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
