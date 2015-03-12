7a1b0823278d94f9035e4702b7382523e093ffc9
Manop Kongoon <kongoon@hotmail.com>
2015-03-12 10:11:23 +0700

    update Report

--- modules/report/views/report/report1.php
+++ modules/report/views/report/report1.php
@@ -3,4 +3,19 @@ use yii\grid\GridView;
 
 echo GridView::widget([
     'dataProvider'=>$dataProvider,
+    'columns'=>[
+        ['class'=>'yii\grid\SerialColumn'],
+        [
+            'label'=>'ปี',
+            'attribute'=>'yy'
+        ],
+        [
+            'label'=>'เดือน',
+            'attribute'=>'mm'
+        ],
+        [
+            'label'=>'จำนวนผู้ป่วยใน',
+            'attribute'=>'cnt'
+        ],
+    ]
 ]);
